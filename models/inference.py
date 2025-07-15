d0f34ca365eaf8802fa7d718c610fec26025451f
Mikhail <gavrilovm2705@gmail.com>
2025-07-15 12:11:26 +0300

    first commit

--- models/inference.py
+++ models/inference.py
@@ -1,27 +1,31 @@
 import torch
-from resnet import model
+from models.resnet import init_model
 from torchvision import transforms
 from PIL import Image
+from typing import Union
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = init_model()
 
-def predict(img):
-    input_tensor = preprocess(img)
+def predict(img_path):
+    img = preprocess(img_path)
     with torch.no_grad():
-        logit = model(input_tensor)
-        prob = logit.softmax(dim=1)
-        pred = prob.argmax(dim=1)
-    return pred
+        input = img.to(device).unsqueeze(0)
+        logit = model(input)
+    # return logit.softmax(dim=1).argmax(dim=1)
+    for name, param in model.named_parameters():
+        print(f"{name}: {param.shape}")
+        print(param.data)
+    return input
 
 
-def preprocess(img):
-    img = Image.open(img)
+def preprocess(img_path: Union[str, bytes]) -> torch.Tensor:
+    img = Image.open(img_path).convert('RGB')
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor()
     ])
-    tensor = transform(img)
-    return tensor.to(device).unsqueeze(0)
+    return transform(img)
 
 
 if __name__ == "__main__":
--- models/resnet.py
+++ models/resnet.py
@@ -2,10 +2,16 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-device = torch.device('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = models.resnet18()
-model.fc = nn.Linear(512, 2)
-model.load_state_dict(torch.load('data/dataset/best_model.pth', map_location=device))
+def init_model():
+    model = models.resnet18()
+    model.fc = nn.Linear(512, 2)
+    model.load_state_dict(torch.load('data/dataset/best_model.pth', map_location=device))
+    model.to(device)
+    model.eval()
+    return model
 
 
+if __name__ == '__main__':
+    model = init_model()
